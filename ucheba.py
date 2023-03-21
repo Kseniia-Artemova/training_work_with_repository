55e4127090cbc353ec1006968ce7b0c4f4192d16
Ksu <kseniyaartemova@yandex.ru>
2023-03-21 17:28:28 +0100

    added gitignore

--- ucheba.py
+++ ucheba.py
@@ -1932,3 +1932,9 @@
 #
 # fish_new = copy.deepcopy(fish)
 
+
+# print("green %s and %s" % ("eggs", "wow"))
+
+# s = '01234567891011121314151617'
+# for i in range(0, len(s), 5):
+#     print(s[i], end='')
